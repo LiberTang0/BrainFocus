9ba76fa6f59c15bfcf39af870f8552333625c173
murkrow92 <ducdt1992@gmail.com>
2017-02-08 01:13:17 +0700

    init work time and break time

--- Assets/script/TimerController.cs
+++ Assets/script/TimerController.cs
@@ -1,23 +1,30 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class TimerController : MonoBehaviour
 {
-    private float countdownTime = 1000f;
-    public Text timerText;
+    private float originWorkTime = 25 * 60 * 1000f;
+    private float originBreakTime = 5 * 60 * 1000f;
+    private float countdownTime;
+
+
+    [SerializeField]
+    private Text timerText;
 
     // Use this for initialization
     void Start()
     {
         timerText = GetComponent<Text>();
+        countdownTime = originWorkTime;
     }
 
     // Update is called once per frame
     void Update()
     {
         countdownTime = countdownTime - Time.deltaTime;
-        print(countdownTime);
+        timerText.text = Math.Round(countdownTime).ToString();
     }
 }
