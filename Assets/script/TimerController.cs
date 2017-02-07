465a348f82b27137cc2c7484e69ea7c6bfc1a473
murkrow92 <ducdt1992@gmail.com>
2017-02-08 00:59:49 +0700

    init timer

--- Assets/script/TimerController.cs
+++ Assets/script/TimerController.cs
@@ -1,16 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class MyTimer : MonoBehaviour {
+public class TimerController : MonoBehaviour
+{
+    private float countdownTime = 1000f;
+    public Text timerText;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+        timerText = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        countdownTime = countdownTime - Time.deltaTime;
+        print(countdownTime);
+    }
 }
